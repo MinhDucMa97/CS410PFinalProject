7ccb0d9366490002a24d933615ef46684d9a6ce7
MinhDucMa97 <95300040+MinhDucMa97@users.noreply.github.com>
2021-11-30 08:01:28 -0800

    First commit

---
+++ Cooking.java
@@ -0,0 +1,15 @@
+//Minh Ma
+//Assignment4 - linked list within an AVL tree - JAVA
+//CS202
+//Cooking class(derived class) implementation
+
+
+package Assignment4;
+
+//Derived Class(Subclass)
+public class Cooking extends Service {
+    //Invoke the constructor of base class
+    public Cooking(String serviceName){
+        super(serviceName);
+    }
+}
---
+++ Tree_Node.java
@@ -0,0 +1,165 @@
+
+//Minh Ma
+//Assignment4 - linked list within an AVL tree - JAVA
+//CS202
+//Tree node implementation
+
+package Assignment4;
+
+import com.sun.source.tree.Tree;
+
+public class Tree_Node {
+    protected Service service;
+    protected int balanceFac;
+    int height;
+
+    protected List_Node head;
+    protected Tree_Node left;
+    protected Tree_Node right;
+
+    public Tree_Node(String type, String serviceName, String providerName, String price, String description){
+        if (type.equals("Delivery")){
+            service = new Delivery(type);
+        }
+        else if(type.equals("Online Tutoring")){
+            service= new Online_Tutor(type);
+        }
+        else if(type.equals("Cooking")){
+            service = new Cooking(type);
+        }
+        head = new List_Node(serviceName,providerName, price, description);
+        this.left = null;
+        this.right = null;
+        this.balanceFac = 0;
+        this.height = 0;
+    }
+
+    public Tree_Node getLeft() {
+        return left;
+    }
+
+
+    public Tree_Node getRight() {
+        return right;
+    }
+
+    //Add a node into an existing type's list
+    public void addListNode(String serviceName,String providerName, String description,String price){
+        addListNode(this.head,serviceName,providerName,description,price);
+    }
+
+    public  void addListNode(List_Node head,String serviceName, String providerName, String description, String price){
+        if(head == null){
+            List_Node temp = new List_Node(serviceName,providerName,description,price);
+            this.head = temp;
+        }
+        if (head.next == null) {
+            List_Node temp = new List_Node(serviceName,providerName,description,price);
+            head.next = temp;
+        } else
+            addListNode(head.next,serviceName, providerName,description,price);
+    }
+
+    //Display all list within a type
+    public void displayList(){
+        if(this.head == null){
+            System.out.println("List does not exist");
+        }
+        else {
+            displayList(head);
+        }
+    }
+
+    protected void displayList(List_Node head){
+        if(head == null){
+            return;
+        }
+        else{
+            head.displayElement();
+            displayList(head.next);
+        }
+    }
+
+    //remove all node in the list
+    public void removeAll_List(){head = null;}
+
+
+    //Remove a single node based on provider name and service name in a list of a type
+    /*public void removeNode_List(String serviceName, String providerName){
+        if(head == null){
+            System.out.println("This list is empty");
+        }
+        if(head.serviceName.equals(serviceName) && head.providerName.equals(providerName)){
+            List_Node temp = this.head;
+            temp.next =
+        }
+        else{
+            removeNode_List(head, serviceName,providerName);
+        }
+    }
+
+
+    public List_Node removeNode_List(List_Node head,String serviceName, String providerName){
+        if(head == null){
+            System.out.println("This item does not exist in this list");
+            return null;
+        }
+        if(serviceName.equals(head.serviceName) && providerName.equals(head.providerName) && head.next == null){
+            return null;
+        }
+        else if(serviceName.equals(head.serviceName) && !providerName.equals(head.providerName) && head.next != null){
+            removeNode_List(head.next,serviceName,providerName);
+        }
+        else if(serviceName.equals(head.serviceName) && providerName.equals(head.providerName) && head.next != null){
+            List_Node temp = head.next;
+            head.next = temp.next;
+
+            return head;
+        }
+        return head;
+    }*/
+
+    public void removeNode_List(String serviceName, String providerName){
+        this.head = removeNode_List(head,serviceName,providerName);
+    }
+
+    protected List_Node removeNode_List(List_Node head, String serviceName, String providerName){
+        if(head == null){
+            System.out.println("The item does not exist.");
+            return null;
+        }
+        if(serviceName.equals(head.serviceName) &&  providerName.equals(head.providerName) && head.next != null){
+            head = head.next;
+            return head;
+        }
+        else if(serviceName.equals(head.serviceName) && providerName.equals(head.providerName) && head.next == null){
+            List_Node temp = head.next;
+            head.next = temp.next;
+            return head;
+        }
+        else if(!serviceName.equals(head.next.serviceName)){
+            head.next =removeNode_List(head.next, serviceName,providerName);
+        }
+        return head;
+
+    }
+
+    public void researchList(String serviceName,String providerName,String description,String price){
+        researchList(head,serviceName,providerName,description,price);
+    }
+
+    public void researchList(List_Node head,String SserviceName, String SproviderName, String Sdescription, String Sprice){
+        if(head == null){
+            return;
+        }
+        if(head.serviceName.compareTo(SserviceName) == 0 && head.providerName.compareTo(SproviderName) == 0
+                && head.description.compareTo(Sdescription) == 0 && head.price.compareTo(Sprice) ==0){
+           head.displayElement();
+
+        }
+        else{
+            researchList(head.next,SserviceName,SproviderName,Sdescription,Sprice);
+        }
+
+    }
+}
